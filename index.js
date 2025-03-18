d2abf2983204acb354667082488742dac7735ae9
victor <victor.mutuma@student.moringaschool.com>
2025-03-18 14:21:54 +0300

    save completed changes

--- index.js
+++ index.js
@@ -1,1 +1,27 @@
 // Code your solution in this file!
+function distanceFromHqInBlocks(street) {
+    return Math.abs(street - 42);
+  }
+  
+  function distanceFromHqInFeet(street) {
+    return distanceFromHqInBlocks(street) * 264;
+  }
+  
+  function distanceTravelledInFeet(start, destination) {
+    return Math.abs(destination - start) * 264;
+  }
+  
+  function calculatesFarePrice(start, destination) {
+    let distance = distanceTravelledInFeet(start, destination);
+  
+    if (distance <= 400) {
+      return 0;
+    } else if (distance > 400 && distance <= 2000) {
+      return (distance - 400) * 0.02;
+    } else if (distance > 2000 && distance <= 2500) {
+      return 25;
+    } else {
+      return "cannot travel that far";
+    }
+  }
+  
